c1dbe78a8ac5b45f2a426e44a12acbe013eb9392
Michelle Li <45521335+floatingturnip@users.noreply.github.com>
2021-04-10 19:28:07 -0700

    added return value

--- scipy_filter.py
+++ scipy_filter.py
@@ -17,13 +17,14 @@ def filtering(values=np.repeat([0., 1., 0.], 100)):
     filtered = signal.convolve(sig, win, mode='same') / sum(win)
     print(filtered)
 
-    plt.figure()
-    plt.plot(sig, 'o')
-    plt.figure()
-    plt.plot(win, 'o')
-    plt.figure()
-    plt.plot(filtered, 'o')
-    plt.show()
+    # plt.figure()
+    # plt.plot(sig, 'o')
+    # plt.figure()
+    # plt.plot(win, 'o')
+    # plt.figure()
+    # plt.plot(filtered, 'o')
+    # plt.show()
+    return filtered
 
 if __name__ == "__main__":
     values = np.array([1.0, 0.5, 0.7, 0.9, 0.8, 0.3, 0.1])
