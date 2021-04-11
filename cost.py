1aa157beece0addda8df97688b788ca0a0ef6a88
sylviazhang9 <sylvia.zhang@alumni.ubc.ca>
2021-04-10 22:17:28 -0700

    added functions to convert between complex and real parameters array

--- cost.py
+++ cost.py
@@ -4,6 +4,30 @@ state_0 = np.array([[1],[0]])
 not_gate = np.array([[0,1],[1,0]])
 h_gate = np.array([[1,1],[1,-1]])/np.sqrt(2)
 
+def real_parameters(complex_array):
+    n = (len(complex_array)-1)/2
+    real_part = np.real(complex_array)
+    imag_part = np.imag(complex_array)
+
+    real_array = np.zeros((4*n+1,))
+    real_array[0] = real_part[0]
+    real_array[1:2*n] = real_part[1:]
+    real_array[2*n+1:] = imag_part[1:]
+
+    return real_array
+
+def complex_parameters(real_array):
+    n = (len(real_array)-1)/4
+    T = real_array[0] + 0j
+    real_part = real_array[1:2*n]
+    imag_part = real_array[2*n+1:]
+    complex_array = np.zeros((2*n+1,))
+
+    complex_array[0] = T
+    complex_array[1:] = real_part + 1j*imag_part
+
+    return complex_array
+
 def signal_concatenate(not_signal, h_signal, pattern):
     '''pattern is an array of 0s and 1s, representing the order of not_signal (0) and h_signal (1) '''
     signal_list = [not_signal, h_signal]
